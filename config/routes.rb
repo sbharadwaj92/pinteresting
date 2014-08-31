18f368359aa3a8f407d2c05122207b1080b34f53
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-09-01 01:21:53 +0530

    Everything is fine

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  get 'pages/home'
+  root "pins#index"
+  get "about" => "pages#about" # Creats about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

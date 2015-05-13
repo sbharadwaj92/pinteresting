40e939bb898e2ef36d20ce66708956f2adc40d06
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2015-05-13 21:57:13 +0530

    updated gems

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140831152607) do
 
-  create_table "pins", force: true do |t|
+  create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20140831152607) do
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
